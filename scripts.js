8e0f4f8e5f8d7d97bedf9703f79c4f84a59912b5
Heitor <m33n1213@gmail.com>
2026-05-24 16:39:32 -0300

    Página de jogo criada, botões para trocar entre as páginas e referencias dos principais elementos do form adicionadas.

---
+++ scripts.js
@@ -0,0 +1,26 @@
+const btnStart = document.getElementById('btnStart');
+const btnIndex = document.getElementById('btnIndex');
+const LabelA = document.getElementById('LabelA');
+const LabelB = document.getElementById('LabelB');
+const LabelC = document.getElementById('LabelC');
+const LabelD = document.getElementById('LabelD');
+const A = document.getElementById('A');
+const B = document.getElementById('B');
+const C = document.getElementById('C');
+const D = document.getElementById('D');
+
+const labels = [LabelA, LabelB, LabelC, LabelD];
+const inputs = [A, B, C, D];
+
+const perguntas = [];
+let voltar = 0;
+
+// Função para mudar entre as páginas
+const redirect = file => window.location.href = file;
+
+// Listeners para as trocas de páginas, o () => redirect() é para não executar o redirect imediatamente, que acontece caso isso não seja feito
+if (btnStart) {btnStart.addEventListener('click', () => redirect('game.html'))}
+if (btnIndex) {btnIndex.addEventListener('click', () => {
+    if (voltar) {redirect('index.html')}
+    else {alert('Você vai perder seu progresso, tem certeza?'); voltar = 1}
+})}
