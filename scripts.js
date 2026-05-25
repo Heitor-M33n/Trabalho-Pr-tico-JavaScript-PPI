ac4f983a4f7db31d586c6182c95cce3aa44244dd
Heitor <m33n1213@gmail.com>
2026-05-24 21:28:17 -0300

    listener de submit adicionado, trabalhando no recebimento de input

--- scripts.js
+++ scripts.js
@@ -1,5 +1,6 @@
 const btnStart = document.getElementById('btnStart');
 const btnIndex = document.getElementById('btnIndex');
+const form = document.getElementById('form')
 const LabelA = document.getElementById('LabelA');
 const LabelB = document.getElementById('LabelB');
 const LabelC = document.getElementById('LabelC');
@@ -15,12 +16,26 @@ const inputs = [A, B, C, D];
 const perguntas = [];
 let voltar = 0;
 
+// Função que manipula o formulário
+function pensar_em_um_nome_function() {
+
+}
+
 // Função para mudar entre as páginas
 const redirect = file => window.location.href = file;
 
 // Listeners para as trocas de páginas, o () => redirect() é para não executar o redirect imediatamente, que acontece caso isso não seja feito
 if (btnStart) {btnStart.addEventListener('click', () => redirect('game.html'))}
+
 if (btnIndex) {btnIndex.addEventListener('click', () => {
     if (voltar) {redirect('index.html')}
-    else {alert('Você vai perder seu progresso, tem certeza?'); voltar = 1}
-})}
+    else {alert('Você vai perder seu progresso, tem certeza?'); voltar = 1}}
+    )
+}
+
+// Listener de enviar resposta da questão
+if (form) {form.addEventListener('submit', (e) => {
+    e.preventDefault(); // necessário para n dar um refresh na página, pois submeter um formulário causa essa ação
+    pensar_em_um_nome_function()}
+    )
+}
